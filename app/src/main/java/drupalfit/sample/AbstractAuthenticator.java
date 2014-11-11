f7e71d4e24e22f902bb0da49cf45cbdfa842e5e4
Andrew Chen <yongjhih@gmail.com>
2014-11-11 11:58:24 +0800

    app: Refactory Authenticator with abstraction

---
+++ app/src/main/java/drupalfit/sample/AbstractAuthenticator.java
@@ -0,0 +1,114 @@
+package drupalfit.sample;
+
+import android.accounts.*;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.app.Activity;
+
+import static android.accounts.AccountManager.KEY_BOOLEAN_RESULT;
+
+import drupalfit.DrupalManager;
+import drupalfit.DrupalOAuth2Manager;
+import drupalfit.DrupalOAuth2.*;
+import drupalfit.DrupalService.*;
+
+public abstract class AbstractAuthenticator extends AbstractAccountAuthenticator {
+
+    private final Context mContext;
+
+    public AbstractAuthenticator(Context context) {
+        super(context);
+
+        // Google - To set mContext as protected!
+        this.mContext = context;
+    }
+
+    @Override
+    public Bundle addAccount(AccountAuthenticatorResponse response, String accountType, String authTokenType, String[] requiredFeatures, Bundle options) throws NetworkErrorException {
+        final Intent intent = new Intent(mContext, getActivityType());
+        intent.putExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE, response);
+
+        final Bundle bundle = new Bundle();
+        bundle.putParcelable(AccountManager.KEY_INTENT, intent);
+        return bundle;
+    }
+
+    @Override
+    public Bundle getAuthToken(AccountAuthenticatorResponse response, Account account, String authTokenType, Bundle options) throws NetworkErrorException {
+        // checkAuthTokenType
+        //result.putString(AccountManager.KEY_ERROR_MESSAGE, "invalid authTokenType");
+
+        // Extract the username and password from the Account Manager, and ask
+        // the server for an appropriate AuthToken.
+        final AccountManager am = AccountManager.get(mContext);
+
+        String authToken = am.peekAuthToken(account, authTokenType);
+
+        // Lets give another try to authenticate the user
+        if (TextUtils.isEmpty(authToken)) {
+            final String password = am.getPassword(account);
+            if (!TextUtils.isEmpty(password)) {
+                try {
+                    authToken = login(account.name, password, authTokenType);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        // If we get an authToken - we return it
+        if (!TextUtils.isEmpty(authToken)) {
+            final Bundle result = new Bundle();
+            result.putString(AccountManager.KEY_ACCOUNT_NAME, account.name);
+            result.putString(AccountManager.KEY_ACCOUNT_TYPE, account.type);
+            result.putString(AccountManager.KEY_AUTHTOKEN, authToken);
+            return result;
+        }
+
+        // If we get here, then we couldn't access the user's password - so we
+        // need to re-prompt them for their credentials. We do that by creating
+        // an intent to display our AuthenticatorActivity.
+        final Intent intent = new Intent(mContext, getActivityType());
+        intent.putExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE, response);
+        final Bundle bundle = new Bundle();
+        bundle.putParcelable(AccountManager.KEY_INTENT, intent);
+        return bundle;
+    }
+
+    /**
+     * @return Activity that will be filled AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE
+     */
+    public abstract Class<? extends Activity> getActivityType();
+
+    /**
+     * @return accessToken
+     */
+    public abstract String login(String username, String password, String authTokenType);
+
+    @Override
+    public String getAuthTokenLabel(String authTokenType) {
+        return null;
+    }
+
+    @Override
+    public Bundle hasFeatures(AccountAuthenticatorResponse response, Account account, String[] features) throws NetworkErrorException {
+        return null;
+    }
+
+    @Override
+    public Bundle editProperties(AccountAuthenticatorResponse response, String accountType) {
+        return null;
+    }
+
+    @Override
+    public Bundle confirmCredentials(AccountAuthenticatorResponse response, Account account, Bundle options) throws NetworkErrorException {
+        return null;
+    }
+
+    @Override
+    public Bundle updateCredentials(AccountAuthenticatorResponse response, Account account, String authTokenType, Bundle options) throws NetworkErrorException {
+        return null;
+    }
+}
